83eb26daa89b29e923bd70c08cc1353ec890e546
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2018-08-29 03:09:18 -0500

    feat: automate math3670

---
+++ math3670/update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copy over notes
+rm -rf ./hw
+mkdir hw
+cp ~/Documents/Github/Vanderbilt/2018-fall/MATH3670/hw/* ./hw
+
+# README title
+printf "## MATH3670: Mathematical Data Science - Doug Hardin\n\n" > index.md
+
+# Reading assignments
+cat ~/Documents/Github/Vanderbilt/2018-fall/MATH3670/reading.md >> index.md
+
+# Homework
+printf "\n\n### Homework\n" >> index.md
+
+for homework in hw/*.pdf
+do
+    printf -- "- ["                                                  >> index.md
+    printf $homework | cut -d'/' -f 2 | cut -d'.' -f 1 | tr -d '\n'   >> index.md
+    printf "](./"                                                    >> index.md
+    printf $homework | cut -d '.' -f 1 | tr -d '\n'                   >> index.md
+    printf ")\n"                                                     >> index.md
+done
+printf "\n" >> index.md
